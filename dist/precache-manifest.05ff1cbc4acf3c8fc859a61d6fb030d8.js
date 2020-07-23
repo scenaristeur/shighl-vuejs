1f368e493dbad1beff1dfd85a89cfa976e6da455
David <scenaristeur@gmail.com>
2020-07-23 21:47:32 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.05ff1cbc4acf3c8fc859a61d6fb030d8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40664da603386a2b15c4",
+    "url": "/css/about.5630e46e.css"
+  },
+  {
+    "revision": "a2df275851f449b04cea",
+    "url": "/css/app.ba014d55.css"
+  },
+  {
+    "revision": "a85f15e3362a1bc47867",
+    "url": "/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b5bf5cf3b52534d567ea220fb4fdc573",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40664da603386a2b15c4",
+    "url": "/js/about.2d521421.js"
+  },
+  {
+    "revision": "a2df275851f449b04cea",
+    "url": "/js/app.0956d9d2.js"
+  },
+  {
+    "revision": "a85f15e3362a1bc47867",
+    "url": "/js/chunk-vendors.5a85b899.js"
+  },
+  {
+    "revision": "7fa297282b8c267a3aec12d403b96aa4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c1da26ce40926be8c75f718871764149",
+    "url": "/vendor/shex-core-browserify.js"
+  },
+  {
+    "revision": "47302de69969e02c51a15979c0b068b1",
+    "url": "/vendor/shex-core-browserify.min.js"
+  },
+  {
+    "revision": "3427d5580c0e5c403891205056694acb",
+    "url": "/vendor/shex-loader-browserify.js"
+  },
+  {
+    "revision": "79a0f6712c55b0717094eb9dadf3f82f",
+    "url": "/vendor/shex-loader-browserify.min.js"
+  },
+  {
+    "revision": "4858ffccba47ae4ba691f2fd52549d9c",
+    "url": "/vendor/shex-parser-browserify.js"
+  },
+  {
+    "revision": "527b3329dbab4eb89e86c2aee67f26f4",
+    "url": "/vendor/shex-parser-browserify.min.js"
+  }
+]);
