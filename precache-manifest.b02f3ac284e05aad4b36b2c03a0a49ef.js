93e56c26bf5169ea572ec30eeb1f228731eccb16
David <scenaristeur@gmail.com>
2020-07-25 16:04:59 +0200

    inputs

---
+++ precache-manifest.b02f3ac284e05aad4b36b2c03a0a49ef.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7ca4b98223698a6cd3",
+    "url": "/shighl-vuejs/css/about.59065b1a.css"
+  },
+  {
+    "revision": "38906bb28a0cf2a592ed",
+    "url": "/shighl-vuejs/css/app.da38b654.css"
+  },
+  {
+    "revision": "0141da7f0cbd3534320f",
+    "url": "/shighl-vuejs/css/chunk-0d62bb54.b5dc4d1b.css"
+  },
+  {
+    "revision": "f8777b88189fcbb6cafc",
+    "url": "/shighl-vuejs/css/chunk-2c44f18b.71b47654.css"
+  },
+  {
+    "revision": "0d0ce2cc00f1b9ba2040",
+    "url": "/shighl-vuejs/css/chunk-32b7d0f8.f784968f.css"
+  },
+  {
+    "revision": "106da3c7d94d86bd50c1",
+    "url": "/shighl-vuejs/css/chunk-350cd064.99e5712b.css"
+  },
+  {
+    "revision": "3691bd824cec85e575ed",
+    "url": "/shighl-vuejs/css/chunk-vendors.cbc2a074.css"
+  },
+  {
+    "revision": "f0af3a5a4c0bc664e1b9abec1d248622",
+    "url": "/shighl-vuejs/examples/loadShape.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/shighl-vuejs/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f979d4cc96fac5d453437d2162ef9300",
+    "url": "/shighl-vuejs/index.html"
+  },
+  {
+    "revision": "4a7ca4b98223698a6cd3",
+    "url": "/shighl-vuejs/js/about.37fbb020.js"
+  },
+  {
+    "revision": "38906bb28a0cf2a592ed",
+    "url": "/shighl-vuejs/js/app.c43deb72.js"
+  },
+  {
+    "revision": "0141da7f0cbd3534320f",
+    "url": "/shighl-vuejs/js/chunk-0d62bb54.6d5a0c12.js"
+  },
+  {
+    "revision": "f8777b88189fcbb6cafc",
+    "url": "/shighl-vuejs/js/chunk-2c44f18b.91a85031.js"
+  },
+  {
+    "revision": "0d0ce2cc00f1b9ba2040",
+    "url": "/shighl-vuejs/js/chunk-32b7d0f8.15a50f31.js"
+  },
+  {
+    "revision": "106da3c7d94d86bd50c1",
+    "url": "/shighl-vuejs/js/chunk-350cd064.fbc358a1.js"
+  },
+  {
+    "revision": "3691bd824cec85e575ed",
+    "url": "/shighl-vuejs/js/chunk-vendors.0bddd264.js"
+  },
+  {
+    "revision": "7fa297282b8c267a3aec12d403b96aa4",
+    "url": "/shighl-vuejs/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/shighl-vuejs/robots.txt"
+  },
+  {
+    "revision": "47302de69969e02c51a15979c0b068b1",
+    "url": "/shighl-vuejs/vendor/shex-core-browserify.min.js"
+  },
+  {
+    "revision": "79a0f6712c55b0717094eb9dadf3f82f",
+    "url": "/shighl-vuejs/vendor/shex-loader-browserify.min.js"
+  }
+]);
